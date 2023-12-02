76e2cc3250877934fa3018a5ba595fc6b89234da
SAAD HUSSAIN <143199003+SAADHUSSAIN31821@users.noreply.github.com>
2023-12-03 02:47:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var pal = prompt("type any word")
+if(pal[0]==pal[pal.length-1] && pal[1]==pal[pal.length-2]){
+    alert("its palandrome word:");
+} else{
+    alert("its not palandrome word:");
+}
